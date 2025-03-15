bc1551dec82620bb2a68819281f9c796c38caaa8
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2025-03-15 19:11:21 +0100

    Updated dependencies versions

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
       script: [
         {
           defer: true,
-          src: "//cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js",
+          src: "//cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js",
         },
       ],
     },
